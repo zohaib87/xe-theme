222e877ff6e7538f49aee52efce6ad93eeb8fae0
Muhammad Zohaib <zohaib87@live.com>
2023-10-04 13:19:30 +0500

    Moved wpbakery check to Helpers class

--- includes/extras.php
+++ includes/extras.php
@@ -180,23 +180,6 @@ if (!function_exists('_xe_custom_search_form')) {
 }
 
 /**
- * # Check if WPBakery plugin is active and used.
- */
-function _xe_bakerycheck() {
-
-  global $post;
-
-  if (in_array('js_composer/js_composer.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-
-    if ($post && preg_match('/vc_row/', $post->post_content)) {
-      return true;
-    }
-
-  }
-
-}
-
-/**
  * # WooCommerce extra functions.
  */
 if ( class_exists('WooCommerce') ) {
